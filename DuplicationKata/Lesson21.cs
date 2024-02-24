f09ebade69ed2d365948e0fd309b1a0ac5f315f7
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-02-24 22:14:02 +0000

    Use braces to create code blocks, and move var declarations

--- DuplicationKata/Lesson21.cs
+++ DuplicationKata/Lesson21.cs
@@ -7,58 +7,60 @@ namespace DuplicationKata
             switch (style)
             {
                 case 1:
-                    
-                    foreach (var name in names)
                     {
                         var dummyBool = true;
                         var nameStart = "L";
                         var specialGreeting = "Hip Hip Hooray! For ";
-                        if (dummyBool && name.StartsWith(nameStart))
+                        foreach (var name in names)
                         {
-                            Sing(specialGreeting + name);
-                        }
-                        else
-                        {
-                            Sing("Hello " + name + ", it's nice to meet you.");
+                            if (dummyBool && name.StartsWith(nameStart))
+                            {
+                                Sing(specialGreeting + name);
+                            }
+                            else
+                            {
+                                Sing("Hello " + name + ", it's nice to meet you.");
+                            }
                         }
                     }
-                    
+
                     break;
                 case 2:
-                    
-                    foreach (var name in names)
                     {
                         var dummyBool = true;
                         var specialGreeting = "Say yeah! Say yo! Say ";
                         var nameStart = "Sam";
-                        if (dummyBool && name.StartsWith(nameStart))
+                        foreach (var name in names)
                         {
-                            Sing(specialGreeting + name);
-                        }
-                        else
-                        {
-                            Sing("Hello " + name + ", it's nice to meet you.");
+                            if (dummyBool && name.StartsWith(nameStart))
+                            {
+                                Sing(specialGreeting + name);
+                            }
+                            else
+                            {
+                                Sing("Hello " + name + ", it's nice to meet you.");
+                            }
                         }
                     }
-                    
+
                     break;
                 case 3:
-                    
-                    foreach (var name in names)
                     {
                         var dummyBool = false;
                         var nameStart = "L";
                         var specialGreeting = "Hip Hip Hooray! For ";
-                        if (dummyBool && name.StartsWith(nameStart))
+                        foreach (var name in names)
                         {
-                            Sing(specialGreeting + name);
-                        }
-                        else
-                        {
-                            Sing("Hello " + name + ", it's nice to meet you.");
+                            if (dummyBool && name.StartsWith(nameStart))
+                            {
+                                Sing(specialGreeting + name);
+                            }
+                            else
+                            {
+                                Sing("Hello " + name + ", it's nice to meet you.");
+                            }
                         }
                     }
-                    
                     break;
             }
         }
