08562d2816c55d3c03dfc4f25594bda95666f891
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-02-24 22:35:25 +0000

    Extract methods

--- DuplicationKata/Lesson31.cs
+++ DuplicationKata/Lesson31.cs
@@ -11,17 +11,7 @@ namespace DuplicationKata
                         var nameStart = "L";
                         var checkNameStart = true;
                         Func<string, string> specialGreetingFunc = name => "Hip Hip Hooray! For ";
-                        foreach (var name in names)
-                        {
-                            if (checkNameStart && name.StartsWith(nameStart))
-                            {
-                                Sing(specialGreetingFunc(name) + name);
-                            }
-                            else
-                            {
-                                Sing("Hello " + name + ", it's nice to meet you.");
-                            }
-                        }
+                        SingRefrain(names, checkNameStart, nameStart, specialGreetingFunc);
                     }
                     break;
                 case 2:
@@ -29,17 +19,7 @@ namespace DuplicationKata
                         var nameStart = "a";
                         var checkNameStart = true;
                         Func<string, string> specialGreetingFunc = name => name.ToUpperInvariant() + "! Yay ";
-                        foreach (var name in names)
-                        {
-                            if (checkNameStart && name.Contains(nameStart))
-                            {
-                                Sing(specialGreetingFunc(name) + name + "!");
-                            }
-                            else
-                            {
-                                Sing("Hello " + name + ", it's nice to meet you.");
-                            }
-                        }
+                        SingRefrain2(names, checkNameStart, nameStart, specialGreetingFunc);
                     }
                     break;
                 case 3:
@@ -47,19 +27,54 @@ namespace DuplicationKata
                     var nameStart = "Don't care";
                     var checkNameStart = false;
                     Func<string, string> specialGreetingFunc = name => "No special greeting";
-                    foreach (var name in names)
-                    {
-                        if (checkNameStart && name.StartsWith(nameStart))
-                        {
-                            Sing(specialGreetingFunc(name) + name);
-                        }
-                        else
-                        {
-                            Sing("Hello " + name + ", it's nice to meet you.");
-                        }
-                    }
+                    SingRefrain3(names, checkNameStart, nameStart, specialGreetingFunc);
                 }
                     break;
+            }
+        }
+
+        private void SingRefrain3(string[] names, bool checkNameStart, string nameStart, Func<string, string> specialGreetingFunc)
+        {
+            foreach (var name in names)
+            {
+                if (checkNameStart && name.StartsWith(nameStart))
+                {
+                    Sing(specialGreetingFunc(name) + name);
+                }
+                else
+                {
+                    Sing("Hello " + name + ", it's nice to meet you.");
+                }
+            }
+        }
+
+        private void SingRefrain2(string[] names, bool checkNameStart, string nameStart, Func<string, string> specialGreetingFunc)
+        {
+            foreach (var name in names)
+            {
+                if (checkNameStart && name.Contains(nameStart))
+                {
+                    Sing(specialGreetingFunc(name) + name + "!");
+                }
+                else
+                {
+                    Sing("Hello " + name + ", it's nice to meet you.");
+                }
+            }
+        }
+
+        private void SingRefrain(string[] names, bool checkNameStart, string nameStart, Func<string, string> specialGreetingFunc)
+        {
+            foreach (var name in names)
+            {
+                if (checkNameStart && name.StartsWith(nameStart))
+                {
+                    Sing(specialGreetingFunc(name) + name);
+                }
+                else
+                {
+                    Sing("Hello " + name + ", it's nice to meet you.");
+                }
             }
         }
     }
