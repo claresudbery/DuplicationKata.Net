93476b33cf1087b4d7a145a0ba3cfb43ef16737d
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-02-24 22:35:58 +0000

    All call same method and remove unused methods

--- DuplicationKata/Lesson31.cs
+++ DuplicationKata/Lesson31.cs
@@ -19,7 +19,7 @@ namespace DuplicationKata
                         var nameStart = "a";
                         var checkNameStart = true;
                         Func<string, string> specialGreetingFunc = name => name.ToUpperInvariant() + "! Yay ";
-                        SingRefrain2(names, checkNameStart, nameStart, specialGreetingFunc);
+                        SingRefrain(names, checkNameStart, nameStart, specialGreetingFunc);
                     }
                     break;
                 case 3:
@@ -27,39 +27,9 @@ namespace DuplicationKata
                     var nameStart = "Don't care";
                     var checkNameStart = false;
                     Func<string, string> specialGreetingFunc = name => "No special greeting";
-                    SingRefrain3(names, checkNameStart, nameStart, specialGreetingFunc);
+                    SingRefrain(names, checkNameStart, nameStart, specialGreetingFunc);
                 }
                     break;
-            }
-        }
-
-        private void SingRefrain3(string[] names, bool checkNameStart, string nameStart, Func<string, string> specialGreetingFunc)
-        {
-            foreach (var name in names)
-            {
-                if (checkNameStart && name.StartsWith(nameStart))
-                {
-                    Sing(specialGreetingFunc(name) + name);
-                }
-                else
-                {
-                    Sing("Hello " + name + ", it's nice to meet you.");
-                }
-            }
-        }
-
-        private void SingRefrain2(string[] names, bool checkNameStart, string nameStart, Func<string, string> specialGreetingFunc)
-        {
-            foreach (var name in names)
-            {
-                if (checkNameStart && name.Contains(nameStart))
-                {
-                    Sing(specialGreetingFunc(name) + name + "!");
-                }
-                else
-                {
-                    Sing("Hello " + name + ", it's nice to meet you.");
-                }
             }
         }
 
