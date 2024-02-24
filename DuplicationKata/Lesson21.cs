b119b6bfe3b9d29cca209198ae0aada5348cc07d
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-02-24 22:17:12 +0000

    All code calls same method, and unused methods are removed

--- DuplicationKata/Lesson21.cs
+++ DuplicationKata/Lesson21.cs
@@ -20,7 +20,7 @@ namespace DuplicationKata
                         var dummyBool = true;
                         var specialGreeting = "Say yeah! Say yo! Say ";
                         var nameStart = "Sam";
-                        SingRefrain2(names, dummyBool, nameStart, specialGreeting);
+                        SingRefrain(names, dummyBool, nameStart, specialGreeting);
                     }
 
                     break;
@@ -29,39 +29,9 @@ namespace DuplicationKata
                         var dummyBool = false;
                         var nameStart = "L";
                         var specialGreeting = "Hip Hip Hooray! For ";
-                        SingRefrain3(names, dummyBool, nameStart, specialGreeting);
+                        SingRefrain(names, dummyBool, nameStart, specialGreeting);
                     }
                     break;
-            }
-        }
-
-        private void SingRefrain3(string[] names, bool dummyBool, string nameStart, string specialGreeting)
-        {
-            foreach (var name in names)
-            {
-                if (dummyBool && name.StartsWith(nameStart))
-                {
-                    Sing(specialGreeting + name);
-                }
-                else
-                {
-                    Sing("Hello " + name + ", it's nice to meet you.");
-                }
-            }
-        }
-
-        private void SingRefrain2(string[] names, bool dummyBool, string nameStart, string specialGreeting)
-        {
-            foreach (var name in names)
-            {
-                if (dummyBool && name.StartsWith(nameStart))
-                {
-                    Sing(specialGreeting + name);
-                }
-                else
-                {
-                    Sing("Hello " + name + ", it's nice to meet you.");
-                }
             }
         }
 
